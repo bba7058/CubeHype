1d1fb92c4cff3af3dd663a84c8869d26a9c1c36a
bba7058 <pisit456987@gmail.com>
2021-08-27 12:07:49 +0700

    finish section tech

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@ get_header();
 					</a>	
 				</div>
 				<div class="col-12 col-lg-6 trending-right">
-					<div class="row h-50 mx-auto">
+					<div class="row mx-auto trending-right-top">
 						<div class="col-12 h-100 px-0 box-right">
 							<a href="#" class="trending-item">
 								<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/jingwei-ke-E7w_Ti4BSSs-unsplash.jpg')?>" class="img-trending" alt="25 Amazing Facts You Didn’t Know About Animals">
@@ -40,8 +40,8 @@ get_header();
 							</a>
 						</div>
 					</div>
-					<div class="row h-50 mx-auto">	
-						<div class="col-6 sm-box h-100">
+					<div class="row mx-auto trending-right-bottom">	
+						<div class="col-sm-6 sm-box">
 							<a href="#" class="trending-item">
 								<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/amirali-mirhashemian-88YAXjnpvrM-unsplash.jpg')?>" class="img-trending" alt="25 Amazing Facts You Didn’t Know About Animals">
 								<div class="trending-header flex-center px-3 py-2">
@@ -49,7 +49,7 @@ get_header();
 								</div>
 							</a>
 						</div>
-						<div class="col-6 sm-box-2 h-100">
+						<div class="col-sm-6 sm-box-2">
 							<a href="#" class="trending-item">
 								<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/zhang-kaiyv-dhiOkqjewAM-unsplash.jpg')?>" class="img-trending" alt="25 Amazing Facts You Didn’t Know About Animals">
 								<div class="trending-header flex-center px-3 py-2">
@@ -79,12 +79,12 @@ get_header();
 											<h3 class="card-title mb-1">BANDAI เปิดตัวของเล่นชิ้นใหม่ “บับเบิ้ลไม่มีที่สิ้นสุด” ช่วยคลายความเครียด แถมการันตีด้วยรางวัล</h3>
 										</a>
 										<div class="info-post">
-											<div class="post-author mr-3">
+											<div class="post-author">
 												<i class="fa fa-user mr-2" aria-hidden="true"></i>
 												Admin
 											</div>
-											<span>|</span>
-											<div class="post-date ml-3">
+											<span class="mx-3">|</span>
+											<div class="post-date">
 												<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
 												3 สิงหาคม 2021
 											</div>
@@ -94,96 +94,200 @@ get_header();
 								</div>
 							</div>
 							<div class="col-md-6">
-							<div class="card mb-3 card-items" style="max-width: 540px;">
-								<div class="row no-gutters">
-									<div class="col-4">
-										<a href="#" class="card-link-img">
-											<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/nrd-a2JMcWOtNAs-unsplash.jpg')?>" class="img-life" alt="">
-										</a>	
+								<div class="card mb-3 card-items" style="max-width: 540px;">
+									<div class="row no-gutters">
+										<div class="col-4">
+											<a href="#" class="card-link-img">
+												<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/nrd-a2JMcWOtNAs-unsplash.jpg')?>" class="img-life" alt="">
+											</a>	
+										</div>
+										<div class="col-8">
+											<div class="card-body py-1 px-2">
+												<a href="#" class="card-link">
+													<h3 class="card-title mb-1">Fall Guys: Ultimate Knockout เตรียมจัดอีเวนต์ครอสโอเวอร์กับ The Jungle Book</h3>
+												</a>
+												<div class="info-post">
+													<div class="post-date">
+														<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
+														3 สิงหาคม 2021
+													</div>
+												</div>
+											</div>
+										</div>
 									</div>
-									<div class="col-8">
-									<div class="card-body py-1 px-2">
+								</div>
+								<div class="card mb-3 card-items" style="max-width: 540px;">
+									<div class="row no-gutters">
+										<div class="col-4">
+											<a href="#" class="card-link-img">
+												<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/gabriel-voltz-xCr7cmReH3Y-unsplash.jpg')?>" class="img-life" alt="">
+											</a>	
+										</div>
+										<div class="col-8">
+											<div class="card-body py-1 px-2">
+												<a href="#" class="card-link">
+													<h3 class="card-title mb-1">Jurassic World Evolution 2 เตรียมวางจำหน่าย 9 พ.ย. นี้</h3>
+												</a>
+												<div class="info-post">
+													<div class="post-date">
+														<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
+														3 สิงหาคม 2021
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card mb-3 card-items" style="max-width: 540px;">
+									<div class="row no-gutters">
+										<div class="col-4">
+											<a href="#" class="card-link-img">
+												<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/jezael-melgoza-43rUwqdoD1g-unsplash.jpg')?>" class="img-life" alt="">
+											</a>	
+										</div>
+										<div class="col-8">
+											<div class="card-body py-1 px-2">
+												<a href="#" class="card-link">
+													<h3 class="card-title mb-1">ทิ้งไว้เป็นตำนาน! ลิโอเนล เมสซี ถูกบีบให้ทิ้งถ้วยรางวัลไว้ที่บาร์เซโลนา</h3>
+												</a>
+												<div class="info-post">
+													<div class="post-date">
+														<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
+														3 สิงหาคม 2021
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="card mb-3 card-items" style="max-width: 540px;">
+										<div class="row no-gutters">
+											<div class="col-4">
+												<a href="#" class="card-link-img">
+													<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/julian-o-hayon-Bs-zngH79Ds-unsplash.jpg')?>" class="img-life" alt="">
+												</a>	
+											</div>
+											<div class="col-8">
+											<div class="card-body py-1 px-2">
+												<a href="#" class="card-link">
+													<h3 class="card-title mb-1">คำเตือนสุดท้ายของ Stephen Hawking เกี่ยวกับโลกและอนาคตที่กำลังจะมาถึง</h3>
+												</a>
+												<div class="info-post">
+													<div class="post-date">
+														<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
+														3 สิงหาคม 2021
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>	
+						</div>
+
+					</section>
+					<section class="section-tech">
+						<div class="header-category">
+							<div class="category-title tech">Technology</div>
+						</div>
+						<div class="row">
+							<div class="col-6">
+								<div class="card border-0 tech-item mb-3">
+									<a href="#" class="card-link-img">
+										<img class="card-img-top img-tech" src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/julian-o-hayon-Bs-zngH79Ds-unsplash.jpg')?>" alt="">
+									</a>
+										<div class="card-body px-0">
 										<a href="#" class="card-link">
-											<h3 class="card-title mb-1">Fall Guys: Ultimate Knockout เตรียมจัดอีเวนต์ครอสโอเวอร์กับ The Jungle Book</h3>
+											<h3 class="card-title mb-1">หนุ่มใช้ AirTag จนเจอสกูตเตอร์ที่โดนขโมยไป</h3>
 										</a>
 										<div class="info-post">
+											<div class="post-author">
+												<i class="fa fa-user mr-2" aria-hidden="true"></i>
+												Admin
+											</div>
+											<span class="mx-3">|</span>
 											<div class="post-date">
 												<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
 												3 สิงหาคม 2021
 											</div>
 										</div>
+										<p class="card-text">เมื่อวานนี้ Evan Blass (@evleaks) ได้โพสต์ภาพเรนเดอร์ของ Fitbit Charge 5 ฟิตเนสแทร็กเกอร์รุ่นใหม่จากฟิตบิท (Fitbit) ภายใต้แบรนด์กูเกิล (Google) ที่จะคาดว่าจะมาพร้อมกับจอสีเป็นรุ่นแรกในไลน์อัป Charge นี้</p>
 									</div>
-								</div>
 								</div>
 							</div>
-							<div class="card mb-3 card-items" style="max-width: 540px;">
-								<div class="row no-gutters">
-									<div class="col-4">
-										<a href="#" class="card-link-img">
-											<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/gabriel-voltz-xCr7cmReH3Y-unsplash.jpg')?>" class="img-life" alt="">
-										</a>	
-									</div>
-									<div class="col-8">
-									<div class="card-body py-1 px-2">
+							<div class="col-6">
+								<div class="card border-0 tech-item mb-3">
+									<a href="#" class="card-link-img">
+										<img class="card-img-top img-tech" src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/zhang-kaiyv-dhiOkqjewAM-unsplash.jpg')?>" alt="">
+									</a>
+										<div class="card-body px-0">
 										<a href="#" class="card-link">
-											<h3 class="card-title mb-1">Jurassic World Evolution 2 เตรียมวางจำหน่าย 9 พ.ย. นี้</h3>
+											<h3 class="card-title mb-1">iPhone และ Mac ในปี 2022 อาจใช้ชิปเทคโนโลยี 3 นาโนเมตร</h3>
 										</a>
 										<div class="info-post">
+											<div class="post-author">
+												<i class="fa fa-user mr-2" aria-hidden="true"></i>
+												Admin
+											</div>
+											<span class="mx-3">|</span>
 											<div class="post-date">
 												<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
 												3 สิงหาคม 2021
 											</div>
 										</div>
+										<p class="card-text">เมื่อวานนี้ Evan Blass (@evleaks) ได้โพสต์ภาพเรนเดอร์ของ Fitbit Charge 5 ฟิตเนสแทร็กเกอร์รุ่นใหม่จากฟิตบิท (Fitbit) ภายใต้แบรนด์กูเกิล (Google) ที่จะคาดว่าจะมาพร้อมกับจอสีเป็นรุ่นแรกในไลน์อัป Charge นี้</p>
 									</div>
-								</div>
 								</div>
 							</div>
-							<div class="card mb-3 card-items" style="max-width: 540px;">
-								<div class="row no-gutters">
-									<div class="col-4">
-										<a href="#" class="card-link-img">
-											<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/jezael-melgoza-43rUwqdoD1g-unsplash.jpg')?>" class="img-life" alt="">
-										</a>	
-									</div>
-									<div class="col-8">
-									<div class="card-body py-1 px-2">
+							<div class="col-6">
+								<div class="card border-0 tech-item mb-3">
+									<a href="#" class="card-link-img">
+										<img class="card-img-top img-tech" src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/alexandre-debieve-FO7JIlwjOtU-unsplash.jpg')?>" alt="">
+									</a>
+										<div class="card-body px-0">
 										<a href="#" class="card-link">
-											<h3 class="card-title mb-1">ทิ้งไว้เป็นตำนาน! ลิโอเนล เมสซี ถูกบีบให้ทิ้งถ้วยรางวัลไว้ที่บาร์เซโลนา</h3>
+											<h3 class="card-title mb-1">AMD Radeon RX 6600 XT ขึ้นแท่นการ์ดจอเหมืองดีที่สุดตอนนี้</h3>
 										</a>
 										<div class="info-post">
+											<div class="post-author">
+												<i class="fa fa-user mr-2" aria-hidden="true"></i>
+												Admin
+											</div>
+											<span class="mx-3">|</span>
 											<div class="post-date">
 												<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
 												3 สิงหาคม 2021
 											</div>
 										</div>
+										<p class="card-text">เมื่อวานนี้ Evan Blass (@evleaks) ได้โพสต์ภาพเรนเดอร์ของ Fitbit Charge 5 ฟิตเนสแทร็กเกอร์รุ่นใหม่จากฟิตบิท (Fitbit) ภายใต้แบรนด์กูเกิล (Google) ที่จะคาดว่าจะมาพร้อมกับจอสีเป็นรุ่นแรกในไลน์อัป Charge นี้</p>
 									</div>
-								</div>
 								</div>
 							</div>
-							<div class="card mb-3 card-items" style="max-width: 540px;">
-								<div class="row no-gutters">
-									<div class="col-4">
-										<a href="#" class="card-link-img">
-											<img src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/julian-o-hayon-Bs-zngH79Ds-unsplash.jpg')?>" class="img-life" alt="">
-										</a>	
-									</div>
-									<div class="col-8">
-									<div class="card-body py-1 px-2">
+							<div class="col-6">
+								<div class="card border-0 tech-item mb-3">
+									<a href="#" class="card-link-img">
+										<img class="card-img-top img-tech" src="<?= esc_url(get_template_directory_uri() .'/assets/img/blog/luca-bravo-XJXWbfSo2f0-unsplash.jpg')?>" alt="">
+									</a>
+										<div class="card-body px-0">
 										<a href="#" class="card-link">
-											<h3 class="card-title mb-1">คำเตือนสุดท้ายของ Stephen Hawking เกี่ยวกับโลกและอนาคตที่กำลังจะมาถึง</h3>
+											<h3 class="card-title mb-1">Zoom ปล่อยฟีเจอร์ Focus mode เน้นให้นักเรียนจับจ้องไปที่ครู ไม่ส่องเพื่อนเสียสมาธิ</h3>
 										</a>
 										<div class="info-post">
+											<div class="post-author">
+												<i class="fa fa-user mr-2" aria-hidden="true"></i>
+												Admin
+											</div>
+											<span class="mx-3">|</span>
 											<div class="post-date">
 												<i class="fa fa-calendar-alt mr-2" aria-hidden="true"></i>
 												3 สิงหาคม 2021
 											</div>
 										</div>
+										<p class="card-text">เมื่อวานนี้ Evan Blass (@evleaks) ได้โพสต์ภาพเรนเดอร์ของ Fitbit Charge 5 ฟิตเนสแทร็กเกอร์รุ่นใหม่จากฟิตบิท (Fitbit) ภายใต้แบรนด์กูเกิล (Google) ที่จะคาดว่าจะมาพร้อมกับจอสีเป็นรุ่นแรกในไลน์อัป Charge นี้</p>
 									</div>
-								</div>
 								</div>
 							</div>
 						</div>
-
 					</section>
 				</div>
 				<div class="col-lg-4"></div>
